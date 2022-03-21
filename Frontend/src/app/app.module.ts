29431630d1fed78d1abe72ea83578463702b94fd
Scifiler-sys <pagdilaostephen@gmail.com>
2022-03-21 12:10:44 -0400

    Added comments

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { PieComponent } from './pie/pie.component';
 
+/*
+  Reference the website link I provided in the website itself to understand what dependencies is needed to be supplied using NPM
+*/
+
 @NgModule({
   declarations: [
     AppComponent,
